c58ed71c47e770ef517fee26c18a3749731aa953
xupeng01 <xupeng451@163.com>
2022-09-14 23:19:05 +0800

    2022.9.14 添加武器

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SG
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        WeaponSlotManager weaponSlotManger;
+        public WeaponItem rightWeapon;
+        public WeaponItem leftWeapon;
+        private void Awake()
+        {
+            weaponSlotManger = GetComponentInChildren<WeaponSlotManager>();
+        }
+        private void Start()
+        {
+            weaponSlotManger.LoadWeaponOnSlot(rightWeapon, false);
+            weaponSlotManger.LoadWeaponOnSlot(leftWeapon, true);
+        }
+    }
+}
+
